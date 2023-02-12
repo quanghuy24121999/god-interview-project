b7fb8b817a7d61831ddf6e5d585dcb4ba9256175
quanghuy24121999 <quanghuy24121999@gmail.com>
2023-02-12 19:41:39 +0700

    init project

---
+++ front-end/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+export default function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>App</div>
+  )
+}
